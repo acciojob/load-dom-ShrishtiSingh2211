602e737581eee6f03795b8d76b93cd409077976c
ShrishtiSingh2211 <142424324+ShrishtiSingh2211@users.noreply.github.com>
2024-03-01 22:16:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+const div=document.createElement("div");
+div.innerText="DOM load success";
+const body=document.getElementsByTagName("body")[0];
+body.insertBefore(div,body);
